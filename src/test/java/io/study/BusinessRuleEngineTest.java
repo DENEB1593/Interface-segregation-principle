70772e2fdccd9953098c8f9a6eb9a536c48991ac
deneb1593 <deneb1593@gmail.com>
2022-12-02 09:36:10 +0900

    action count 단위테스트 추가

--- src/test/java/io/study/BusinessRuleEngineTest.java
+++ src/test/java/io/study/BusinessRuleEngineTest.java
@@ -2,10 +2,23 @@ package io.study;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 class BusinessRuleEngineTest {
+
+  @Test
+  void shouldCountWithTwoActions() {
+    Facts mockFacts = mock(Facts.class);
+    Action mockAction = mock(Action.class);
+    BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine(mockFacts);
+
+    businessRuleEngine.addAction(mockAction);
+    businessRuleEngine.addAction(mockAction);
+
+    assertEquals(2, businessRuleEngine.count());
+  }
 
   @Test
   void shouldPerformAnActionWithFacts() {
